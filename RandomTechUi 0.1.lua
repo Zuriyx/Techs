5bc5e4a08f931138aae72cf83ce8349faf807bd3
Zuriyx <r3k33551@gmail.com>
2026-06-27 00:08:55 -0600

    Create RandomTechUi 0.1.lua

---
+++ RandomTechUi 0.1.lua
@@ -0,0 +1,9 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+
+local PhantomForcesWindow = Library:NewWindow("Made by Zuri")
+
+local LegitTechs = PhantomForcesWindow:NewSection("Legit Techs")
+
+LegitTechs:CreateButton("Supa (Beta)", function()
+    loadstring(game:HttpGet("https://github.com/Zuriyx/Techs/raw/refs/heads/main/Supa.txt"))()
+end)
